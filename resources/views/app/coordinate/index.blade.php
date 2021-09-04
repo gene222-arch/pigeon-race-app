2b0f131523315d6f6375f53967f1e7080d33e92a
gene222-arch <genephillip222@gmail.com>
2021-09-04 16:23:46 +0800

    feat: added table

--- resources/views/app/coordinate/index.blade.php
+++ resources/views/app/coordinate/index.blade.php
@@ -1,5 +1,27 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h1>Coordinates</h1>
+<div class="container-fluid mt-5">
+    <div class="card">
+      <div class="card-header bg-warning">
+          <h5>View Coordinates</h5>
+      </div>
+      <table class="table table-hover">
+          <thead class="thead-dark">
+              <tr>
+                  <th scope="col">Name</th>
+                  <th scope="col">Coordinates</th>
+                  <th scope="col">Distance</th>
+              </tr>
+          </thead>
+          <tbody>
+              <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+              </tr>
+          </tbody>
+      </table>
+  </div>
+</div>
 @endsection
