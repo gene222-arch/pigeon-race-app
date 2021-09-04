ab8661ecf209167c02c6d09e557259929681112b
gene222-arch <genephillip222@gmail.com>
2021-09-04 16:29:48 +0800

    feat: added table

--- resources/views/app/my-pigeon/index.blade.php
+++ resources/views/app/my-pigeon/index.blade.php
@@ -1,5 +1,37 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h1>My Pigeons</h1>
+<div class="container-fluid mt-5">
+    <div class="card">
+      <div class="card-header bg-warning">
+          <h5>My Pigeons</h5>
+      </div>
+      <table class="table table-hover">
+          <thead class="thead-dark">
+              <tr>
+                  <th scope="col">Action</th>
+                  <th scope="col">Photo</th>
+                  <th scope="col">Ring Band#</th>
+                  <th scope="col">Color or Gender</th>
+                  <th scope="col">Remarks</th>
+                  <th scope="col">Blood Line</th>
+                  <th scope="col">Status</th>
+                  <th scope="col">Date Created</th>
+              </tr>
+          </thead>
+          <tbody>
+              <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+              </tr>
+          </tbody>
+      </table>
+  </div>
+</div>
 @endsection
