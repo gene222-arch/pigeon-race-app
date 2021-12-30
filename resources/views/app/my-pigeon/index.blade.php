db280896247eb25898b2d05834b4cf7af37c2213
gene222-arch <genephillip222@gmail.com>
2021-12-31 00:01:38 +0800

    style: add responsiveness to table

--- resources/views/app/my-pigeon/index.blade.php
+++ resources/views/app/my-pigeon/index.blade.php
@@ -25,68 +25,70 @@
       <div class="card-header bg-warning">
           <h5>My Pigeons</h5>
       </div>
-      <table class="table table-hover">
-          <thead class="thead-dark">
-              <tr>
-                  <th scope="col">Action</th>
-                  <th scope="col">Photo</th>
-                  <th scope="col">Ring Band#</th>
-                  <th scope="col">Color or Gender</th>
-                  <th scope="col">Remarks</th>
-                  <th scope="col">Blood Line</th>
-                  <th scope="col">Status</th>
-                  <th scope="col">Date Created</th>
-              </tr>
-          </thead>
-          <tbody>
-            @forelse ($myPigeons as $myPigeon)
+      <div class="table-responsive">
+        <table class="table table-hover">
+            <thead class="thead-dark">
                 <tr>
-                    <td class="text-center">
-                        <a type="button" class="btn btn-info btn-block" href="{{ route('my-pigeons.show', [ 'my_pigeon' => $myPigeon->id ]) }}">
-                            <i class="fas fa-eye text-white"></i>
-                        </a>
-                        @hasrole('User')
-                            <a type="button" class="btn btn-warning btn-block" href="{{ route('my-pigeons.edit', [ 'my_pigeon' => $myPigeon->id ]) }}">
-                                <i class="fas fa-edit"></i>
-                            </a>
-                        @endhasrole
-                        @hasrole('User')
-                            <form method="POST" action="{{ route('my-pigeons.destroy', $myPigeon->id) }}">
-                                @csrf
-                                @method('DELETE')
-                            
-                                <div class="form-group my-2">
-                                    <button 
-                                        type="submit" 
-                                        class="btn btn-danger btn-block"
-                                    >
-                                        <i class="fas fa-trash"></i>
-                                    </button>
-                                </div>
-                            </form>
-                        @endhasrole
-                    </td>
-                    <td>
-                        <img src="{{ $myPigeon->image_path }}" width="100" height="100" alt="">
-                    </td>
-                    <td>
-                        {{ $myPigeon->ring_band }}
-                    </td>
-                    <td>
-                        {{ $myPigeon->color . ' ' . $myPigeon->gender }}
-                    </td>
-                    <td>{{ $myPigeon->remarks }}</td>
-                    <td>{{ $myPigeon->bloodline }}</td>
-                    <td>
-                        <span class="badge badge-success">{{ $myPigeon->status }}</span>
-                    </td>
-                    <td>{{ $myPigeon->created_at }}</td>
+                    <th scope="col">Action</th>
+                    <th scope="col">Photo</th>
+                    <th scope="col">Ring Band#</th>
+                    <th scope="col">Color or Gender</th>
+                    <th scope="col">Remarks</th>
+                    <th scope="col">Blood Line</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Date Created</th>
                 </tr>
-            @empty
-                
-            @endforelse
-          </tbody>
-      </table>
+            </thead>
+            <tbody>
+              @forelse ($myPigeons as $myPigeon)
+                  <tr>
+                      <td class="text-center">
+                          <a type="button" class="btn btn-info btn-block" href="{{ route('my-pigeons.show', [ 'my_pigeon' => $myPigeon->id ]) }}">
+                              <i class="fas fa-eye text-white"></i>
+                          </a>
+                          @hasrole('User')
+                              <a type="button" class="btn btn-warning btn-block" href="{{ route('my-pigeons.edit', [ 'my_pigeon' => $myPigeon->id ]) }}">
+                                  <i class="fas fa-edit"></i>
+                              </a>
+                          @endhasrole
+                          @hasrole('User')
+                              <form method="POST" action="{{ route('my-pigeons.destroy', $myPigeon->id) }}">
+                                  @csrf
+                                  @method('DELETE')
+                              
+                                  <div class="form-group my-2">
+                                      <button 
+                                          type="submit" 
+                                          class="btn btn-danger btn-block"
+                                      >
+                                          <i class="fas fa-trash"></i>
+                                      </button>
+                                  </div>
+                              </form>
+                          @endhasrole
+                      </td>
+                      <td>
+                          <img src="{{ $myPigeon->image_path }}" width="100" height="100" alt="">
+                      </td>
+                      <td>
+                          {{ $myPigeon->ring_band }}
+                      </td>
+                      <td>
+                          {{ $myPigeon->color . ' ' . $myPigeon->gender }}
+                      </td>
+                      <td>{{ $myPigeon->remarks }}</td>
+                      <td>{{ $myPigeon->bloodline }}</td>
+                      <td>
+                          <span class="badge badge-success">{{ $myPigeon->status }}</span>
+                      </td>
+                      <td>{{ $myPigeon->created_at }}</td>
+                  </tr>
+              @empty
+                  
+              @endforelse
+            </tbody>
+        </table>
+      </div>
   </div>
 </div>
 @endsection
